1756c68ddf3fdb92c7d06c8442859248a9b166d2
Franco Cuya <francocuya13@gmail.com>
2025-05-25 20:30:27 -0500

    Tests

--- src/test/java/com/francocuya13/elimapassspring/ELimaPassSpringApplicationTests.java
+++ src/test/java/com/francocuya13/elimapassspring/ELimaPassSpringApplicationTests.java
@@ -1,9 +1,13 @@
 package com.francocuya13.elimapassspring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 class ELimaPassSpringApplicationTests {
 
     @Test
